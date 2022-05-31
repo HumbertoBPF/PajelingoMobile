45a36182f7c3f780c0af3a483cb6b86baf0b78be
HumbertoBPF <humbertobpf@gmail.com>
2022-05-31 10:06:31 -0300

    :lipstick: Adding splashscreen and changing app icon :lipstick:

---
+++ app/src/main/java/com/example/pajelingo/activities/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.pajelingo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.pajelingo.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
