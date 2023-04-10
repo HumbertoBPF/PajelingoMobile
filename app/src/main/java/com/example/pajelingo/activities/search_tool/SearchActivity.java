fbcec12cc8f7bcee4c93eb465a95b2de9b413d7e
HumbertoBPF <humbertobpf@gmail.com>
2023-04-10 10:19:02 -0300

    :sparkles: :construction: :children_crossing: Adding an icon to the SearchActivity (old SearchResultsActivity) to set search filters :children_crossing: :construction: :sparkles:

--- app/src/main/java/com/example/pajelingo/activities/search_tool/SearchActivity.java
+++ app/src/main/java/com/example/pajelingo/activities/search_tool/SearchActivity.java
@@ -1,74 +1,167 @@
 package com.example.pajelingo.activities.search_tool;
 
+import static com.example.pajelingo.utils.Tools.displayFavoriteWordError;
+import static com.example.pajelingo.utils.Tools.getAuthToken;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pajelingo.R;
-import com.example.pajelingo.daos.LanguageDao;
+import com.example.pajelingo.adapters.SearchResultsAdapter;
+import com.example.pajelingo.daos.WordDao;
 import com.example.pajelingo.database.settings.AppDatabase;
-import com.example.pajelingo.models.Language;
-import com.example.pajelingo.ui.LabeledSpinner;
+import com.example.pajelingo.models.FavoriteWordPayload;
+import com.example.pajelingo.models.Word;
+import com.example.pajelingo.retrofit.LanguageSchoolAPI;
+import com.example.pajelingo.retrofit.LanguageSchoolAPIHelper;
 
-public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private EditText searchEditText;
-    private Spinner languageSpinner;
-    private Button searchButton;
+import java.util.ArrayList;
+import java.util.List;
 
-    private LanguageDao languageDao;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class SearchActivity extends AppCompatActivity {
+
+    private ConstraintLayout warningConstraintLayout;
+    private ImageView warningImageView;
+    private TextView warningTextView;
+    private RecyclerView resultsRecyclerView;
+    private WordDao wordDao;
+    private LanguageSchoolAPI languageSchoolAPI;
     private String selectedLanguage;
+    private String pattern;
+    private SearchResultsAdapter adapter;
+
+    private ActivityResultLauncher<Intent> startActivityIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_search_results);
 
         setTitle(R.string.dictionary_activity_title);
 
-        LabeledSpinner languageInput = findViewById(R.id.language_input);
+        warningConstraintLayout = findViewById(R.id.warning_constraint_layout);
+        warningImageView = findViewById(R.id.warning_image_view);
+        warningTextView = findViewById(R.id.warning_text_view);
+        resultsRecyclerView = findViewById(R.id.search_recycler_view);
 
-        searchEditText = findViewById(R.id.search_edit_text);
-        languageSpinner = languageInput.getSpinner();
-        searchButton = findViewById(R.id.search_button);
+        wordDao = AppDatabase.getInstance(this).getWordDao();
 
-        languageDao = AppDatabase.getInstance(this).getLanguageDao();
+        languageSchoolAPI = LanguageSchoolAPIHelper.getApiObject();
 
+        pattern = "";
         selectedLanguage = getString(R.string.all_languages_spinner_option);
 
-        languageDao.getAllRecordsTask(result -> {
-            result.add(new Language(getString(R.string.all_languages_spinner_option)));
-            ArrayAdapter<Language> adapter = new ArrayAdapter<>(SearchActivity.this,
-                    android.R.layout.simple_spinner_item, result);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            languageSpinner.setAdapter(adapter);
-            languageSpinner.setOnItemSelectedListener(SearchActivity.this);
-            int lastPosition = languageSpinner.getAdapter().getCount();
-            languageSpinner.setSelection(lastPosition - 1);
-        }).execute();
+        startActivityIntent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            Intent intent = result.getData();
 
-        searchButton.setOnClickListener(v -> {
-            Intent intent = new Intent(SearchActivity.this, SearchResultsActivity.class);
-            intent.putExtra("pattern", searchEditText.getText().toString());
-            intent.putExtra("selectedLanguage", selectedLanguage);
-            startActivity(intent);
+            if (intent == null) {
+                return;
+            }
+
+            pattern = intent.getStringExtra("pattern");
+            selectedLanguage = intent.getStringExtra("selectedLanguage");
         });
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        selectedLanguage = languageSpinner.getSelectedItem().toString();
+    protected void onResume() {
+        super.onResume();
+        searchWords();
+    }
+
+    private void searchWords() {
+        showWarning(R.drawable.loading, R.string.loading_message);
+
+        if (selectedLanguage.equals(getString(R.string.all_languages_spinner_option))){
+            wordDao.searchWordsTask("%" + pattern + "%", this::populateRecyclerView).execute();
+        }else{
+            wordDao.searchWordsTask("%" + pattern + "%", selectedLanguage, this::populateRecyclerView).execute();
+        }
+    }
+
+    private void showWarning(int warningImage, int warningText) {
+        warningConstraintLayout.setVisibility(View.VISIBLE);
+        warningImageView.setImageResource(warningImage);
+        warningTextView.setText(warningText);
+        resultsRecyclerView.setVisibility(View.GONE);
+    }
+
+    private void populateRecyclerView(List<Word> words){
+        new Handler().postDelayed(() -> {
+            if (words.size() == 0){
+                showWarning(R.drawable.no_result, R.string.no_results_message);
+            }else{
+                warningConstraintLayout.setVisibility(View.GONE);
+                resultsRecyclerView.setVisibility(View.VISIBLE);
+            }
+
+            adapter = new SearchResultsAdapter(SearchActivity.this, words, (word, position) -> {
+                Boolean isFavorite = word.getFavorite();
+                FavoriteWordPayload payload =  new FavoriteWordPayload(!isFavorite);
+                Call<Word> call = languageSchoolAPI.favoriteWord(getAuthToken(SearchActivity.this), payload, word.getId());
+
+                call.enqueue(new Callback<Word>() {
+                    @Override
+                    public void onResponse(Call<Word> call, Response<Word> response) {
+                        Word returnedWord = response.body();
+
+                        if ((response.isSuccessful()) && (returnedWord != null)) {
+                            List<Word> wordList = new ArrayList<>();
+                            wordList.add(returnedWord);
+                            wordDao.getSaveAsyncTask(wordList, result -> {
+                                adapter.setWordAtPosition(returnedWord, position);
+                            }).execute();
+                        }else {
+                            displayFavoriteWordError(SearchActivity.this, word);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Word> call, Throwable t) {
+                        Toast.makeText(SearchActivity.this, R.string.warning_connection_error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            });
+
+            resultsRecyclerView.setAdapter(adapter);
+        }, 2000);
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int selectedItemId = item.getItemId();
+
+        if (selectedItemId == R.id.action_filter) {
+            Intent intent = new Intent(this, SearchWithFiltersActivity.class);
+            intent.putExtra("pattern", pattern);
+            intent.putExtra("selectedLanguage", selectedLanguage);
+            startActivityIntent.launch(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/pajelingo/activities/search_tool/SearchWithFiltersActivity.java
@@ -0,0 +1,101 @@
+package com.example.pajelingo.activities.search_tool;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pajelingo.R;
+import com.example.pajelingo.daos.LanguageDao;
+import com.example.pajelingo.database.settings.AppDatabase;
+import com.example.pajelingo.models.Language;
+import com.example.pajelingo.ui.LabeledSpinner;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SearchWithFiltersActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private EditText searchEditText;
+    private Spinner languageSpinner;
+    private Button searchButton;
+
+    private LanguageDao languageDao;
+
+    private String selectedLanguage;
+
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        setTitle(R.string.dictionary_activity_title);
+
+        LabeledSpinner languageInput = findViewById(R.id.language_input);
+
+        searchEditText = findViewById(R.id.search_edit_text);
+        languageSpinner = languageInput.getSpinner();
+        searchButton = findViewById(R.id.search_button);
+
+        languageDao = AppDatabase.getInstance(this).getLanguageDao();
+
+        intent = getIntent();
+
+        if (intent != null) {
+            String pattern = intent.getStringExtra("pattern");
+            searchEditText.setText(pattern);
+        }
+
+        languageDao.getAllRecordsTask(result -> {
+            result.add(new Language(getString(R.string.all_languages_spinner_option)));
+            ArrayAdapter<Language> adapter = new ArrayAdapter<>(SearchWithFiltersActivity.this,
+                    android.R.layout.simple_spinner_item, result);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            languageSpinner.setAdapter(adapter);
+            languageSpinner.setOnItemSelectedListener(SearchWithFiltersActivity.this);
+            int position = getLanguageItemPosition(result);
+            languageSpinner.setSelection(position);
+        }).execute();
+
+        searchButton.setOnClickListener(v -> {
+            Intent resultIntent = new Intent(SearchWithFiltersActivity.this, SearchActivity.class);
+            resultIntent.putExtra("pattern", searchEditText.getText().toString());
+            resultIntent.putExtra("selectedLanguage", selectedLanguage);
+            setResult(RESULT_OK, resultIntent);
+            finish();
+        });
+    }
+
+    private int getLanguageItemPosition(List<Language> languages) {
+        if (intent != null) {
+            String selectedLanguage = intent.getStringExtra("selectedLanguage");
+
+            for (int i = 0;i < languages.size();i++) {
+                Language language = languages.get(i);
+
+                if (Objects.equals(selectedLanguage, language.getLanguageName())){
+                    return i;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        selectedLanguage = languageSpinner.getSelectedItem().toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
