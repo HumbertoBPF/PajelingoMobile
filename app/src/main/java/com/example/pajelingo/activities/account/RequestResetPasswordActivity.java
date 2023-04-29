10e44081d648732f9fccb4cbfe11be7aa89da5c7
HumbertoBPF <humbertobpf@gmail.com>
2023-04-29 15:13:50 -0300

    :lipstick: :children_crossing: Using loading button on the RequestResetPasswordActivity :children_crossing: :lipstick:

--- app/src/main/java/com/example/pajelingo/activities/account/RequestResetPasswordActivity.java
+++ app/src/main/java/com/example/pajelingo/activities/account/RequestResetPasswordActivity.java
@@ -4,7 +4,6 @@ import static android.util.Patterns.EMAIL_ADDRESS;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.pajelingo.R;
 import com.example.pajelingo.models.ResetEmail;
 import com.example.pajelingo.retrofit.LanguageSchoolAPIHelper;
+import com.example.pajelingo.ui.LoadingButton;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,7 +23,7 @@ import retrofit2.Response;
 public class RequestResetPasswordActivity extends AppCompatActivity {
 
     private EditText emailEditText;
-    private Button resetPasswordButton;
+    private LoadingButton resetPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,22 +41,14 @@ public class RequestResetPasswordActivity extends AppCompatActivity {
             }else if (!EMAIL_ADDRESS.matcher(email).matches()){
                 Toast.makeText(RequestResetPasswordActivity.this, R.string.warning_email_format_required, Toast.LENGTH_SHORT).show();
             }else{
-                submitRequestResetAccount(email);
+                requestResetAccount(email);
             }
         });
     }
 
-    private void submitRequestResetAccount(CharSequence email) {
-        AlertDialog loadingDialog = new AlertDialog.Builder(RequestResetPasswordActivity.this).setTitle(R.string.reset_password_loading_dialog_title)
-                .setMessage(R.string.reset_password_loading_dialog_message)
-                .setCancelable(false).create();
+    private void requestResetAccount(CharSequence email) {
+        resetPasswordButton.setLoading(true);
 
-        loadingDialog.show();
-
-        new Handler().postDelayed(() -> requestResetAccount(email, loadingDialog), 3000);
-    }
-
-    private void requestResetAccount(CharSequence email, AlertDialog loadingDialog) {
         AlertDialog feedbackDialog = new AlertDialog.Builder(RequestResetPasswordActivity.this).setTitle(R.string.reset_password_feedback_dialog_title)
                 .setMessage(R.string.reset_password_feedback_dialog_message)
                 .setPositiveButton(R.string.reset_password_positive_button_text, (dialog, which) -> {
@@ -70,7 +62,7 @@ public class RequestResetPasswordActivity extends AppCompatActivity {
             call.enqueue(new Callback<Void>() {
                 @Override
                 public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
-                    loadingDialog.dismiss();
+                    resetPasswordButton.setLoading(false);
                     if (!response.isSuccessful()){
                         feedbackDialog.setTitle(R.string.reset_password_error_dialog_title);
                         feedbackDialog.setMessage(getString(R.string.warning_connection_error));
@@ -80,7 +72,7 @@ public class RequestResetPasswordActivity extends AppCompatActivity {
 
                 @Override
                 public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
-                    loadingDialog.dismiss();
+                    resetPasswordButton.setLoading(false);
                     feedbackDialog.setTitle(R.string.reset_password_error_dialog_title);
                     feedbackDialog.setMessage(getString(R.string.warning_connection_error));
                     feedbackDialog.show();
